7f2a7219e33d7cc91820cd07622dd833a686e952
sstiijn <22836503+sstiijn@users.noreply.github.com>
2025-03-08 19:20:36 +0100

    Add snack save (#1)

---
+++ snackbox/snacks/save/tests/fakes.py
@@ -0,0 +1,23 @@
+from snackbox.snacks.save.save import BaseGitCommmitter, BaseTestRunner
+
+
+class FakeTestRunner(BaseTestRunner):
+
+    def __init__(self, passes: bool):
+        self.__passes = passes
+
+    def run_tests(self) -> bool:
+        """Fake implementation for unit tests"""
+        print("running tests")
+        return self.__passes
+
+
+class FakeGitCommitter(BaseGitCommmitter):
+
+    def __init__(self):
+        self.committed = False
+
+    def commit(self) -> None:
+        """Fake implementation for unit tests"""
+        print("Commit")
+        self.committed = True
